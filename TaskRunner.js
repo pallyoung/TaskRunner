8b9f46c44417f8a2fc07eada0789a116a85931c1
Spencer <pallyoung@163.com>
2016-03-04 20:49:39 +0800

    bug require

--- TaskRunner.js
+++ TaskRunner.js
@@ -1,37 +1,56 @@
-"use strict"
-class TaskRunner {
-  constructor(tasks) {
+
+(function (){
+  "use strict"
+  function TaskRunner(tasks){
     this.tasks = tasks ||[];
     this.status = 0;
     this.indicator = [];
   }
-  _taskFinished(cb){
-    this.indicator.pop();
-    if(this.indicator.length == 0){
-      cb();
+  TaskRunner.prototype = {
+    _taskFinished:function(cb){
+      this.indicator.pop();
+      if(this.indicator.length == 0){
+        cb();
+      }
+    },
+    run:function(){
+      var tasks = this.tasks;
+      var result = null;
+      var self = this;
+      return new Promise(function(resolve,reject){
+        if(tasks.length===0){
+          resolve();
+          return ;
+        }
+        for(var i = 0;i<tasks.length;i++){
+          self.indicator.push(true);
+           try{
+              result = tasks[i].call();
+           }catch(e){
+            console.log(e);
+           }
+          if(result&& (result instanceof Promise ||typeof result.then==="function") ){
+            result.then(function(){
+              self._taskFinished(resolve);
+            });
+          }else{
+            self._taskFinished(resolve);
+          }
+        }
+      });
+    },
+    addTask:function(task){
+      this.tasks.push(task);
     }
   }
-  run(){
-    var tasks = this.tasks;
-    var cb = cb||function(){};
-    var result = null;
-    var self = this;
-    return new Promise(function(resolve,reject){
-      for(var i = 0;i<tasks.length;i++){
-        this.indicator.push(true);
-        result = tasks[i].call();
-        if(result instanceof Promise ){
-          result.then(function(){
-            self._taskFinished(resolve);
-          })
-        }else{
-          self._taskFinished(resolve);
-        }
-      }
+  if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return TaskRunner;
     });
+  } else if (typeof exports !== 'undefined') {
+    exports.TaskRunner = TaskRunner;
+  } else {
+    window.TaskRunner = TaskRunner;
   }
-  addTask(task){
-    this.tasks.push(task);
-  }
-}
-module.exports = TaskRunner;
+})()
+
